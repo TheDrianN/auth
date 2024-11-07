d746a5fd86c78c178b18c3cefef88abc7b93d97a
TheDrianN <edelgadovidarte@gmail.com>
2024-11-07 17:41:09 -0500

    update desarrollo

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,18 +3,21 @@ import * as joi from 'joi'
 
 interface EnvVars{
     PORT: number;
+    PORT_MC: number;
     SMTP_HOST: string;
     SMTP_PORT: string;
     SMTP_USER: string;
     SMTP_PASS: string;
     SECRET_KEY: string;
+    DATABASE_URL: string;
+
    
 }
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
-   
-    
+    PORT_MC: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
 })
 
 
@@ -30,6 +33,8 @@ const EnvVars: EnvVars = value;
 
 export const envs = {
     port: EnvVars.PORT,
+    port_mc: EnvVars.PORT_MC,
+    databaseUrl: EnvVars.DATABASE_URL,
     smtp_host: EnvVars.SMTP_HOST,
     smtp_port: EnvVars.SMTP_PORT,
     smtp_user: EnvVars.SMTP_USER,
